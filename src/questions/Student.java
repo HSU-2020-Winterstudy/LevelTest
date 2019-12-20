1f243f30dee613ad1011d30063fd88df1d1a04c4
Arakene <kbh97102@naver.com>
2019-12-20 12:51:08 +0900

    Q4 : Resolve Abstract problem in Student Class

--- src/questions/Student.java
+++ src/questions/Student.java
@@ -8,6 +8,16 @@ public class Student extends Person {
     public Student(String name, String sex){
         super(name, sex);
     }
+
+    @Override
+    public void work() {
+        System.out.println("I'm studying");
+    }
+
+    @Override
+    public void eat() {
+        System.out.println("kukbab");
+    }
     /**
      * ID's getter in Person class is already return Int
      */
