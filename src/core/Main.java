84a37104f4ae74680cd079596ce7b77cd527d84f
Arakene <kbh97102@naver.com>
2019-12-20 21:03:48 +0900

    Q9 : Check contains Speed

--- src/core/Main.java
+++ src/core/Main.java
@@ -10,80 +10,76 @@ public class Main {
     public static void main(String[] args) {
         DynamicArray<Student> dynamicStudents = new DynamicArray<>(Student.class);
         ArrayList<Student> listStudents = new ArrayList<>();
-        HashMap<String,Student> mapStudents = new HashMap<>();
+        HashMap<String, Student> mapStudents = new HashMap<>();
 
         ArrayList<Student> randomTable_1 = new ArrayList<>();
         ArrayList<Student> randomTable_2 = new ArrayList<>();
 
         //randomTables
-        for(int i=0;i<70000;i++){
+        for (int i = 0; i < 30000; i++) {
             Student student = Student.autoGenerator();
             int randomID = (int) (Math.random() * 200000);
             student.setID(randomID);
             randomTable_1.add(student);
         }
-        for(int i=0;i<70000;i++){
+        for (int i = 0; i < 30000; i++) {
             Student student = Student.autoGenerator();
             int randomID = (int) (Math.random() * 200000);
             student.setID(randomID);
             randomTable_2.add(student);
         }
 
-        for(int i=0;i<70000;i++){
+        for (int i = 0; i < 30000; i++) {
             Student student = Student.autoGenerator();
             int randomID = (int) (Math.random() * 200000);
             student.setID(randomID);
-            mapStudents.put(student.getName(),student);
+            mapStudents.put(student.getName(), student);
         }
-        for(int i=0;i<70000;i++){
+        for (int i = 0; i < 30000; i++) {
             Student student = Student.autoGenerator();
             int randomID = (int) (Math.random() * 200000);
             student.setID(randomID);
             dynamicStudents.put(student);
         }
-        for(int i=0;i<70000;i++){
+        for (int i = 0; i < 30000; i++) {
             Student student = Student.autoGenerator();
             int randomID = (int) (Math.random() * 200000);
             student.setID(randomID);
             listStudents.add(student);
         }
         System.out.println("Inserting Finish");
+        int dynamicSuccessCount = 0;
+        int listSuccessCount = 0;
+        int mapSuccessCount = 0;
         long startTime = System.currentTimeMillis();
-        for(int i=0;i<randomTable_1.size();i++){
-            for(int x=0;x<dynamicStudents.size();x++){
-                if(dynamicStudents.get(x).getID() == randomTable_1.get(x).getID()){
-                    dynamicStudents.remove(x);
-                    break;
-                }
+        for (int i = 0; i < randomTable_2.size(); i++) {
+            if (dynamicStudents.contains(randomTable_2.get(i)) != -1) {
+                dynamicSuccessCount++;
             }
-            System.out.println(i);
         }
         long endTime = System.currentTimeMillis();
-        System.out.println("Dynamic "+(endTime-startTime)/1000+"초");
+        System.out.println("Dynamic " + (endTime - startTime) / 1000 + "초");
 
         startTime = System.currentTimeMillis();
-        for(int i=0;i<randomTable_1.size();i++){
-            for(int x=0;x<listStudents.size();x++){
-                if(listStudents.get(x).getID() == randomTable_1.get(x).getID()){
-                    listStudents.remove(x);
-                    break;
-                }
+        for (int i = 0; i < randomTable_2.size(); i++) {
+            if (listStudents.contains(randomTable_2.get(i))) {
+                listSuccessCount++;
             }
         }
         endTime = System.currentTimeMillis();
-        System.out.println("List "+(endTime-startTime)/1000+"초");
+        System.out.println("List " + (endTime - startTime) / 1000 + "초");
 
         startTime = System.currentTimeMillis();
-        for(int i=0;i<randomTable_1.size();i++){
-            String randomName = randomTable_1.get(i).getName();
-            for(int x=0;x<mapStudents.size();x++){
-                if(mapStudents.get(randomName)!=null && mapStudents.get(randomName).getID() == randomTable_1.get(x).getID()){
-                    mapStudents.remove(randomName);
-                    break;
-                }
+        for (int i = 0; i < randomTable_2.size(); i++) {
+            if (mapStudents.containsValue(randomTable_2.get(i))) {
+                mapSuccessCount++;
             }
         }
         endTime = System.currentTimeMillis();
-        System.out.println("Map "+(endTime-startTime)/1000+"초");
+        System.out.println("Map " + (endTime - startTime) / 1000 + "초");
+
+        System.out.println("DynamicSuccess " + dynamicSuccessCount);
+        System.out.println("ListSuccess " + listSuccessCount);
+        System.out.println("MapSuccess " + mapSuccessCount);
     }
 }
