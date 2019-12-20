2b81c68f9b735c572f4027b349424b2dfa5fe37a
Arakene <kbh97102@naver.com>
2019-12-20 09:26:19 +0900

    Q1 : Add docs

--- src/question_1/Person.java
+++ src/question_1/Person.java
@@ -5,25 +5,48 @@ public class Person {
     private String name;
     private String sex;
 
+    /**
+     * @param ID = Person's identify number
+     * @param name = Person's name
+     * @param sex = Person's sex (man or woman)
+     */
     public Person(int ID, String name, String sex){
         this.ID = ID;
         this.name = name;
         this.sex = sex;
     }
+
+    /**
+     * ID = Initialize -1
+     * @param name = Person's name
+     * @param sex = Person's sex (man or woman)
+     */
     public Person(String name, String sex){
         this.name = name;
         this.sex = sex;
         this.ID = -1;
     }
 
+    /**
+     *
+     * @return Person's identify number
+     */
     public int getID() {
         return ID;
     }
 
+    /**
+     *
+     * @return Person's name
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     *
+     * @return Person's sex (man or woman)
+     */
     public String getSex() {
         return sex;
     }
