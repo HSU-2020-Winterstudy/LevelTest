1503ae27702c05d8fbeec09420b8ff3f7475cf57
Arakene <kbh97102@naver.com>
2019-12-20 13:03:37 +0900

    Q5 : Update PartTimer extends Person

--- src/questions/PartTimer.java
+++ src/questions/PartTimer.java
@@ -1,8 +1,26 @@
 package questions;
 
-public class PartTimer implements PartTimeJob{
+public class PartTimer extends Person implements PartTimeJob{
+    public PartTimer(int ID, String name, String sex) {
+        super(ID, name, sex);
+    }
+
+    public PartTimer(String name, String sex) {
+        super(name, sex);
+    }
+
     @Override
     public void workPartTime() {
         System.out.println("PartTimer workPartTime");
+    }
+
+    @Override
+    public void work() {
+        System.out.println("PartTimer work");
+    }
+
+    @Override
+    public void eat() {
+        System.out.println("PartTimer eat");
     }
 }
