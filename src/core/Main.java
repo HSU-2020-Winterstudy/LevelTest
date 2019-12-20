3180ac3ef6be698c941247d4169ea3aa361639be
Arakene <kbh97102@naver.com>
2019-12-20 18:39:12 +0900

    Q8 : Add fields and Constructor

--- src/core/Main.java
+++ src/core/Main.java
@@ -5,69 +5,6 @@ import questions.*;
 
 public class Main {
     public static void main(String[] args) {
-        Student student_1 = Student.autoGenerator();
-        Student student_2 = Student.autoGenerator();
-        Student student_3 = Student.autoGenerator();
-        PartTimer partTimer_1 = PartTimer.autoGenerator();
-        PartTimer partTimer_2 = PartTimer.autoGenerator();
-        PartTimer partTimer_3 = PartTimer.autoGenerator();
 
-
-        System.out.println(student_1.toString());
-        System.out.println(student_2.toString());
-        System.out.println(student_3.toString());
-        System.out.println(partTimer_1.toString());
-        System.out.println(partTimer_2.toString());
-        System.out.println(partTimer_3.toString());
-
-//        student_1.work();
-//        student_1.workPartTime();
-//        student_1.eat();
-//
-//        student_2.work();
-//        student_2.workPartTime();
-//        student_2.eat();
-//
-//        student_3.work();
-//        student_3.workPartTime();
-//        student_3.eat();
-//
-//        partTimer_1.work();
-//        partTimer_1.workPartTime();
-//        partTimer_1.eat();
-//
-//        partTimer_2.work();
-//        partTimer_2.workPartTime();
-//        partTimer_2.eat();
-//
-//        partTimer_3.work();
-//        partTimer_3.workPartTime();
-//        partTimer_3.eat();
-
-        PartTimeJob[] partTimeJobs = new PartTimeJob[6];
-        for(int i=0;i<3;i++){
-            partTimeJobs[i] = (PartTimeJob)Student.autoGenerator();
-        }
-        for(int i=3;i<6;i++){
-            partTimeJobs[i] = (PartTimeJob)PartTimer.autoGenerator();
-        }
-
-        for(int i=0;i<6;i++){
-            partTimeJobs[i].workPartTime();
-        }
-        for(PartTimeJob partTimeJob : partTimeJobs){
-            partTimeJob.workPartTime();
-        }
-
-        int count=0;
-        while(count < partTimeJobs.length){
-            partTimeJobs[count++].workPartTime();
-        }
-        /**
-         *  Abstract Class can't be instance because that has not initialized methods
-         **/
-        //Person person_1 = Person.autoGenerator();
-//        Person person_2 = Person.autoGenerator();
-//        Person person_3 = Person.autoGenerator();
     }
 }
---
+++ src/questions/DynamicArray.java
@@ -0,0 +1,21 @@
+package questions;
+
+import java.lang.reflect.Array;
+
+public class DynamicArray<T> {
+    private T[] dynamicArray;
+    private int currentSize;
+    private int resizeDegree;
+    private int defaultSize = 10;
+    private int nodes;
+    private Class<T> type;
+
+    @SuppressWarnings("unchecked")
+    public DynamicArray(Class<T> type){
+        this.type = type;
+        currentSize = defaultSize;
+        resizeDegree = 2;
+        nodes = 0;
+        dynamicArray = (T[])Array.newInstance(type,defaultSize);
+    }
+}
