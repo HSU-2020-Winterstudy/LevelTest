d8d15ec673863d32f2af441d5679bd9a5ebe79bd
Arakene <kbh97102@naver.com>
2019-12-20 18:50:38 +0900

    Q8 : DynamicArray Check

--- src/core/Main.java
+++ src/core/Main.java
@@ -5,6 +5,13 @@ import questions.*;
 
 public class Main {
     public static void main(String[] args) {
-
+        DynamicArray<PartTimeJob> partTimerJobs = new DynamicArray<>(PartTimeJob.class);
+        for(int i=0;i<100000;i++){
+            partTimerJobs.put(PartTimer.autoGenerator());
+        }
+        for(int i=0;i<partTimerJobs.size();i++){
+            System.out.print(i+" ");
+            partTimerJobs.get(i).workPartTime();
+        }
     }
 }
