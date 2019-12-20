b850cb2a8a64325ed58461194bbe45fa0fa9d2b1
Arakene <kbh97102@naver.com>
2019-12-20 12:58:02 +0900

    Q5 : Create PartTimer implements PartTimeJob

---
+++ src/questions/PartTimer.java
@@ -0,0 +1,8 @@
+package questions;
+
+public class PartTimer implements PartTimeJob{
+    @Override
+    public void workPartTime() {
+        System.out.println("PartTimer workPartTime");
+    }
+}
