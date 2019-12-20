d2e48e0a33ddf17ff52557a0a18b528390bbf01a
Arakene <kbh97102@naver.com>
2019-12-20 18:56:58 +0900

    Q9 : Add students and Check Speed

--- src/core/Main.java
+++ src/core/Main.java
@@ -3,15 +3,35 @@ package core;
 
 import questions.*;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Main {
     public static void main(String[] args) {
-        DynamicArray<PartTimeJob> partTimerJobs = new DynamicArray<>(PartTimeJob.class);
-        for(int i=0;i<100000;i++){
-            partTimerJobs.put(PartTimer.autoGenerator());
+        DynamicArray<Student> dynamicStudents = new DynamicArray<>(Student.class);
+        ArrayList<Student> listStudents = new ArrayList<>();
+        HashMap<String,Student> mapStudents = new HashMap<>();
+
+        Student student = Student.autoGenerator();
+        long startTime = System.currentTimeMillis();
+        for(int i=0;i<200000000;i++){
+            dynamicStudents.put(student);
         }
-        for(int i=0;i<partTimerJobs.size();i++){
-            System.out.print(i+" ");
-            partTimerJobs.get(i).workPartTime();
+        long endTime = System.currentTimeMillis();
+        System.out.println("Dynamic "+(endTime-startTime)/1000+"초");
+
+        startTime = System.currentTimeMillis();
+        for(int i=0;i<200000000;i++){
+            listStudents.add(student);
         }
+        endTime = System.currentTimeMillis();
+        System.out.println("List "+(endTime-startTime)/1000+"초");
+
+        startTime = System.currentTimeMillis();
+        for(int i=0;i<200000000;i++){
+            mapStudents.put(student.getName(),student);
+        }
+        endTime = System.currentTimeMillis();
+        System.out.println("Map "+(endTime-startTime)/1000+"초");
     }
 }
