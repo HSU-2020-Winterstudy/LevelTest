f144081b61a64beeaa360a574d489292ffb070c4
Arakene <kbh97102@naver.com>
2019-12-20 12:37:50 +0900

    Q2 : Add docs

--- src/question_2/AutoIDGenerator.java
+++ src/question_2/AutoIDGenerator.java
@@ -4,6 +4,13 @@ import question_1.Person;
 
 public class AutoIDGenerator {
     private static int nextID = 10;
+
+    /**
+     *
+     * @param ID Person's identify number
+     * @return If Person's ID is equal nextID return true,
+     *         else return false and Person's ID = -1
+     */
     public static boolean checkIDIsEqual(int ID){
         if(ID != nextID){
             return false;
@@ -11,9 +18,20 @@ public class AutoIDGenerator {
         nextID++;
         return true;
     }
+
+    /**
+     *
+     * @return NextID and Increase
+     */
     public static int getNextID(){
         return nextID++;
     }
+
+    /**
+     *
+     * @param person Person object
+     * @return If Person's ID is between -1 and NextID return true (this object is safe)
+     */
     public static boolean checkIsOk(Person person){
         if(-1 < person.getID() && person.getID() < nextID){
             return true;
