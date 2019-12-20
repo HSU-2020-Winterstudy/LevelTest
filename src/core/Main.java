232371155a08a9c2f40fd6a0f30d055599f69866
Arakene <kbh97102@naver.com>
2019-12-20 17:52:18 +0900

    Q7 : Instantiate Person, Student, PartTimer

--- src/core/Main.java
+++ src/core/Main.java
@@ -1,7 +1,24 @@
 package core;
 
 
+import questions.PartTimer;
+import questions.Person;
+import questions.Student;
+
 public class Main {
     public static void main(String[] args) {
+        Student student_1 = Student.autoGenerator();
+        Student student_2 = Student.autoGenerator();
+        Student student_3 = Student.autoGenerator();
+        PartTimer partTimer_1 = PartTimer.autoGenerator();
+        PartTimer partTimer_2 = PartTimer.autoGenerator();
+        PartTimer partTimer_3 = PartTimer.autoGenerator();
+
+        /**
+         *  Abstract Class can't be instance because that has not initialized methods
+         **/
+        //Person person_1 = Person.autoGenerator();
+//        Person person_2 = Person.autoGenerator();
+//        Person person_3 = Person.autoGenerator();
     }
 }
