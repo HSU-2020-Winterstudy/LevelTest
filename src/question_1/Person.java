dc11b21d46126b7626174e28ff7192cd1e46bebb
Arakene <kbh97102@naver.com>
2019-12-20 09:14:50 +0900

    Q1 : Add 3 fields

---
+++ src/question_1/Person.java
@@ -0,0 +1,13 @@
+package question_1;
+
+public class Person {
+    private int ID;
+    private String name;
+    private String sex;
+
+    public Person(int ID, String name, String sex){
+        this.ID = ID;
+        this.name = name;
+        this.sex = sex;
+    }
+}
