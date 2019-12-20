6a41d65e1db0c047062353f229d6d59f88cb7243
Arakene <kbh97102@naver.com>
2019-12-20 17:40:32 +0900

    Q6 : Add Person and Studt AutoGenerator

--- src/questions/Person.java
+++ src/questions/Person.java
@@ -1,21 +1,22 @@
 package questions;
 
 
+import java.util.Random;
+
 public abstract class Person {
     private int ID;
     private String name;
     private String sex;
 
     /**
-     * @param ID = Person's identify number
+     * @param ID   = Person's identify number
      * @param name = Person's name
-     * @param sex = Person's sex (man or woman)
+     * @param sex  = Person's sex (man or woman)
      */
-    public Person(int ID, String name, String sex){
-        if(AutoIDGenerator.checkIDIsEqual(ID)){
+    public Person(int ID, String name, String sex) {
+        if (AutoIDGenerator.checkIDIsEqual(ID)) {
             this.ID = ID;
-        }
-        else{
+        } else {
             this.ID = -1;
         }
         this.name = name;
@@ -24,10 +25,11 @@ public abstract class Person {
 
     /**
      * ID = Initialize -1
+     *
      * @param name = Person's name
-     * @param sex = Person's sex (man or woman)
+     * @param sex  = Person's sex (man or woman)
      */
-    public Person(String name, String sex){
+    public Person(String name, String sex) {
         this.ID = AutoIDGenerator.getNextID();
         this.name = name;
         this.sex = sex;
@@ -35,7 +37,6 @@ public abstract class Person {
     }
 
     /**
-     *
      * @return Person's identify number
      */
     public int getID() {
@@ -43,7 +44,6 @@ public abstract class Person {
     }
 
     /**
-     *
      * @return Person's name
      */
     public String getName() {
@@ -51,22 +51,53 @@ public abstract class Person {
     }
 
     /**
-     *
      * @param name Person's Name
      */
-    public void setName(String name){
+    public void setName(String name) {
         this.name = name;
     }
+
     /**
-     *
      * @return Person's sex (man or woman)
      */
     public String getSex() {
         return sex;
     }
 
-
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
 
     abstract public void work();
+
     abstract public void eat();
+
+    public static Person autoGenerator() {
+
+        String className;
+        String hashCode;
+        String sex;
+        Random random = new Random(System.currentTimeMillis());
+        int randomSex = random.nextInt(2);
+
+        if (randomSex == 1) {
+            sex = "men";
+        } else {
+            sex = "woman";
+        }
+        Person person = null;
+        try {
+            Class<?> classInfo = Person.class;
+            className = classInfo.getName();
+            person = (Person) classInfo.newInstance();
+            hashCode = Integer.toHexString(person.hashCode()).toUpperCase();
+
+            person.setName(className + "-" + hashCode);
+            person.setSex(sex);
+
+        } catch (IllegalAccessException | InstantiationException e1) {
+            e1.printStackTrace();
+        }
+        return person;
+    }
 }
--- src/questions/Student.java
+++ src/questions/Student.java
@@ -1,6 +1,8 @@
 package questions;
 
 
+import java.util.Random;
+
 public class Student extends Person implements PartTimeJob{
     public Student(int ID, String name, String sex) {
         super(ID, name, sex);
@@ -26,4 +28,26 @@ public class Student extends Person implements PartTimeJob{
     /**
      * ID's getter in Person class is already return Int
      */
+
+    public static Student autoGenerator(){
+        String className;
+        String hashCode;
+        String sex;
+        Random random = new Random(System.currentTimeMillis());
+        int randomSex = random.nextInt(2);
+
+        if(randomSex == 1){
+            sex = "men";
+        }
+        else {
+            sex = "woman";
+        }
+        Student student = new Student("",sex);
+        Class<?> classInfo = Student.class;
+        className = classInfo.getName();
+        hashCode = Integer.toHexString(student.hashCode()).toUpperCase();
+
+        student.setName(className+"-"+hashCode);
+        return student;
+    }
 }
