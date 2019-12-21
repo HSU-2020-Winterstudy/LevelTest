490eba2c9619a24d3173a26680dda2dc6d45d8cf
pllapallpal <pllapallpal@gmail.com>
2019-12-21 17:34:04 +0900

    Q3: Add Student.java which extends Person.java

---
+++ src/person/Student.java
@@ -0,0 +1,53 @@
+package person;
+
+/**
+ *
+ * Author: pllapallpal
+ * Created date: 2019-12-21
+ */
+public class Student extends Person {
+
+    // variables ID and nextID are hiding the member variables ID and nextID of Student's superclass
+    private int ID;
+    private static int nextID;
+
+    /**
+     *
+     * @param ID
+     * @param name
+     * @param gender
+     */
+    public Student(int ID, String name, String gender) {
+
+        super(name, gender);
+
+        this.ID = ID;
+        if (nextID == ID) {
+            this.ID = nextID++;
+        }
+        else {
+            this.ID = -1;
+        }
+    }
+
+    /**
+     *
+     * @param name
+     * @param gender
+     */
+    public Student(String name, String gender) {
+
+        super(name, gender);
+
+        this.ID = nextID++;
+    }
+
+    /**
+     *
+     * @return the ID of the student
+     */
+    @Override
+    public int getID() {
+        return this.ID;
+    }
+}
