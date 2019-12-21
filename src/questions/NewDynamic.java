b20f9e8dc651a37ba9e8f12d7f5c7a8b5fe7619e
Arakene <kbh97102@naver.com>
2019-12-21 14:21:37 +0900

    Q12 : Add extend method

--- src/questions/NewDynamic.java
+++ src/questions/NewDynamic.java
@@ -1,5 +1,7 @@
 package questions;
 
+import java.lang.reflect.Array;
+
 public class NewDynamic <T> {
 
     private Object[] arr;
@@ -13,12 +15,40 @@ public class NewDynamic <T> {
     }
 
     public void put(T data){
-        arr[nodes] = data;
-        nodes++;
+        if(nodes >= size){
+            extend();
+        }
+        arr[nodes++] = data;
     }
 
     @SuppressWarnings("unchecked")
     public T get(int index){
         return (T)arr[index];
+    }
+
+    public int size(){
+        return nodes;
+    }
+
+    public void extend(){
+        try{
+            Object [] temp = new Object[size];
+            size = size *2;
+            if (nodes >= 0) System.arraycopy(arr, 0, temp, 0, nodes);
+            arr = new Object[size];
+            if (nodes >= 0) System.arraycopy(temp, 0, arr, 0, nodes);
+        }
+        catch (IndexOutOfBoundsException e1){
+            System.out.println("Index Error");
+            e1.printStackTrace();
+        }
+        catch (ArrayStoreException e2){
+            System.out.println("Type mismatch");
+            e2.printStackTrace();
+        }
+        catch (NullPointerException e3){
+            System.out.println("Friendly NullPointerException");
+            e3.printStackTrace();
+        }
     }
 }
