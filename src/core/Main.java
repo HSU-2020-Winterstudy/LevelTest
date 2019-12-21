671e386102a19c0f8070264e2598c1d97e97ecc6
Arakene <kbh97102@naver.com>
2019-12-21 11:42:59 +0900

    Q10 : Extract Students's Name at thirdPlace and Make them String

--- src/core/Main.java
+++ src/core/Main.java
@@ -4,6 +4,7 @@ package core;
 import questions.*;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 
 public class Main {
@@ -14,15 +15,13 @@ public class Main {
         for(int i=0;i<100;i++){
             students.add(Student.autoGenerator());
         }
-
-        for(Student student : students){
-            StringBuilder studentName = new StringBuilder(student.getName());
-            studentName.setCharAt(0,'A');
-            student.setName(studentName.toString());
+        char [] getNameAtThird = new char[students.size()];
+        for(int i=0;i<students.size();i++){
+            getNameAtThird[i] = students.get(i).getName().charAt(2);
         }
+        System.out.println(students.get(1).getName());
+        String result = String.valueOf(getNameAtThird);
 
-        for(Student student : students){
-            System.out.println(student.getName());
-        }
+        System.out.println(result);
     }
 }
