b410b7de30fd125f60d5abe09be89efbbd32ecea
unnokid <kim_0609@naver.com>
2019-12-21 17:16:05 +0900

    Q2: update Person

--- docs/src/Q1.java
+++ docs/src/Q1.java
@@ -1,6 +1,6 @@
-/* Q1.md
+/* Q2.md
  * Author : unnokid
- *  created date : 2019-12-21
+ * created date : 2019-12-21
  */
 
 public class Person
@@ -8,11 +8,21 @@ public class Person
 	private int ID;
 	private String name;
 	private String gender;
+	private static int nextID;
 	
 	//1번 생성자
 	public Person(int ID, String name, String gender)
 	{
-		this.ID= ID;
+		
+		if(nextID == ID)
+		{
+			this.ID=ID;
+		}
+		else
+		{
+			this.ID=-1;
+		}
+
 		this.name=name;
 		this.gender=gender;
 	}
@@ -22,7 +32,8 @@ public class Person
 	{
 		this.name=name;
 		this.gender=gender;
-		this.ID =-1;
+		this.ID =nextID;
+		nextID++;
 	}
 	
 	//getter 메서드 추가
@@ -39,6 +50,14 @@ public class Person
 	public String getGender()
 	{
 		return gender;
+	}
+
+	public static boolean correct(Person person)
+	{
+		if(person.ID >-1 && person.ID < next ID)
+			return true;
+		else
+			return false;
 	}
 
 
