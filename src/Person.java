59973cc09978d60b12e772d3911613ad4b8f933a
Disarmy <56792210+Disarmy@users.noreply.github.com>
2019-12-22 21:35:00 +0900

    Q2: Update Person class

--- src/Person.java
+++ src/Person.java
@@ -1,36 +1,57 @@
 /**
  * Author : Disarmy
- * Created date : 2019-12-20
+ * Created date : 2019-12-22
  */
 
-public class Person{
+public class Person {
     private int id = -1;
     private String name;
     private String gender;
+    private static int nextID;
 
-    //1번 생성자
-   public Person(int id, String name, String gender){
-        this.id = id;
+    /*
+     * First Constructor */
+    public Person(int id, String name, String gender) {
+        if (id != nextID)
+            this.id = -1;
+        else
+            this.id = id;
         this.name = name;
         this.gender = gender;
     }
 
-    //2번 생성자
-    public Person(String name, String gender){
-       this.name = name;
-       this.gender = gender;
+    /*
+     * Second Constructor */
+    public Person(String name, String gender) {
+        this.id = nextID++;
+        this.name = name;
+        this.gender = gender;
     }
 
-    public int getId(){
-       return id;
+    /*
+     * @return Person's id */
+    public int getId() {
+        return id;
     }
 
+    /*
+     * @return Person's name */
     public String getName() {
         return name;
     }
 
+    /*
+     * @return Person's gender */
     public String getGender() {
         return gender;
     }
 
+    /*
+     * @return Person is correct*/
+    public static boolean isCorrect() {
+        if (this.id > -1 && this.id < nextID)
+            return true;
+        else
+            return false;
+    }
 }
