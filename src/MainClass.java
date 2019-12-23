612bf7fd347ff553145e70d673becfba257feb41
pllapallpal <pllapallpal@gmail.com>
2019-12-23 19:26:25 +0900

    Q10: Solve Q10

--- src/MainClass.java
+++ src/MainClass.java
@@ -5,12 +5,18 @@ public class MainClass {
     public static void main(String[] args) {
 
         Student[] students = new Student[100];
-        for (Student student : students) {
-            student = Student.generateStudent();
-            StringBuilder newName = new StringBuilder(student.getName());
-            newName.setCharAt(0, 'A');
-            student.setName(newName.toString());
-            System.out.println(student.getName());
+        for (int i = 0; i < 100; ++i) {
+            students[i] = Student.generateStudent();
+            StringBuilder newName = new StringBuilder(students[i].getName());
+            newName.setCharAt(8, 'A');
+            students[i].setName(newName.toString());
+            System.out.println(students[i].getName());
         }
+
+        StringBuilder thirdChar = new StringBuilder();
+        for (Student student : students) {
+            thirdChar.append(student.getName().charAt(10));
+        }
+        System.out.println(thirdChar);
     }
 }
