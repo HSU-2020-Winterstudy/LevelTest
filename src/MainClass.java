19450c3eeda5e9b5e3af105466ab3b2fb186140e
pllapallpal <pllapallpal@gmail.com>
2019-12-23 16:09:02 +0900

    Q7: Update MainClass for test

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,9 +1,29 @@
+import person.PartTimer;
 import person.Person;
+import person.Student;
 
 public class MainClass {
 
     public static void main(String[] args) {
 
+        System.out.println("ID name gender");
 
+        Student[] students = new Student[3];
+        for (int i = 0; i < 3; ++i) {
+            students[i] = new Student("student " + Integer.toString(i), true);
+            System.out.println(students[i].getID() + " " + students[i].getName() + " " + students[i].getGender());
+        }
+
+        PartTimer[] partTimers = new PartTimer[3];
+        for (int i = 0; i < 3; ++i) {
+            partTimers[i] = new PartTimer("partTimer " + Integer.toString(i), true);
+            System.out.println(partTimers[i].getID() + " " + partTimers[i].getName() + " " + partTimers[i].getGender());
+        }
+
+        /**
+         *
+         * instantiation of Person - error occurs
+         */
+//        Person person = new Person(" ", true);
     }
 }
