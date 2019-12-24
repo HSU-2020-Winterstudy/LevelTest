a427a2794b1a20a83af1aa17efd1ec290ba2bc1d
Disarmy <56792210+Disarmy@users.noreply.github.com>
2019-12-24 21:49:17 +0900

    Q10: Solve the problem

--- src/MainClass.java
+++ src/MainClass.java
@@ -5,77 +5,21 @@
 
 public class MainClass {
     public static void main(String[] args) {
-        /* error
-        Person person = new Person(" ", " ");
-         */
+        Student[] students = new Student[100];
 
-        Student[] students = new Student[3];
-        PartTimer[] partTimers = new PartTimer[3];
-
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < 100; i++) {
             students[i] = Student.randomGenerator();
-            partTimers[i] = PartTimer.randomGenerator();
+            StringBuilder insertA = new StringBuilder(students[i].getName());
+            insertA.setCharAt(8, 'A');
+            students[i].setName(insertA.toString());
+
+            System.out.println(students[i].getName());
         }
 
-        //Student test
-        for (int i = 0; i < 3; i++) {
-            System.out.println("=============");
-            System.out.println("Student name: " + students[i].getName());
-            System.out.println("Student id: " + students[i].getId());
-            System.out.println("Student gender: " + students[i].getGender());
-            System.out.println("Student isCorrect: " + students[i].isCorrect(students[i]));
-            System.out.println("Student nextId: " + students[i].getNextID());
-            System.out.print("Student work: ");
-            students[i].work();
-            System.out.print("Student eat: ");
-            students[i].eat();
-            System.out.print("Student workPartTime: ");
-            students[i].workPartTime();
+        StringBuilder third = new StringBuilder();
+        for (int i = 0; i < 100; i++) {
+            third.append(students[i].getName().charAt(10));
         }
-
-        //PartTimer test
-        for (int i = 0; i < 3; i++) {
-            System.out.println("=============");
-            System.out.println("PartTimer name: " + partTimers[i].getName());
-            System.out.println("PartTimer id: " + partTimers[i].getId());
-            System.out.println("PartTimer gender: " + partTimers[i].getGender());
-            System.out.println("PartTimer isCorrect: " + partTimers[i].isCorrect(partTimers[i]));
-            System.out.println("PartTimer nextId: " + partTimers[i].getNextID());
-            System.out.print("PartTimer work: ");
-            partTimers[i].work();
-            System.out.print("PartTimer eat: ");
-            partTimers[i].eat();
-            System.out.print("PartTimer workPartTime: ");
-            partTimers[i].workPartTime();
-        }
-
-        //PartTimerJob test
-
-        PartTimeJob[] partTimeJobs = new PartTimeJob[6];
-
-        for (int i = 0; i < 3; i++) {
-            partTimeJobs[i] = students[i];
-            partTimeJobs[i + 3] = partTimers[i];
-        }
-
-        //for test
-        System.out.println("=============");
-        for (int i = 0; i < 6; i++) {
-            partTimeJobs[i].workPartTime();
-        }
-
-        //while test
-        System.out.println("=============");
-        int i = 0;
-        while (i < 6) {
-            partTimeJobs[i].workPartTime();
-            i++;
-        }
-
-        //for-each test
-        System.out.println("=============");
-        for(PartTimeJob test : partTimeJobs){
-            test.workPartTime();
-        }
+        System.out.println(third.toString());
     }
 }
